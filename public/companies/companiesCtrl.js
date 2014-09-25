7ebbffd6fb340c4cf795c3a0e1f9d00b20f48494
crotten <wildrunts@gmail.com>
2014-09-24 22:10:26 -0400

    working on the ability to add locations to map with address

--- public/companies/companiesCtrl.js
+++ public/companies/companiesCtrl.js
@@ -35,37 +35,54 @@
               },
               zoom: 11
             }
-            $scope.options = {scrollwheel: true};
+            // $scope.options = {scrollwheel: true};
+            //
+            // $scope.companyMarkers = [];
+            //
+            // $scope.addMarker = function (marker) {
+            //    var idKey = $scope.companyMarkers.length ? 0 : $scope.companyMarkers.length;
+            //
+            //     $scope.companyMarkers.push({
+            //         id: idKey,
+            //         coords: {
+            //         latitude: $scope.marker.markerLat,
+            //         longitude: $scope.marker.markerLng
+            //       }
+            //     });
+            //
+            //     console.log('Maker add: ' + $scope.companyMarkers);
+            //     $scope.markerLat ="";
+            //     $scope.markerLng ="";
+            // };
 
-            $scope.companyMarkers = [];
-
-            $scope.addMarker = function (marker) {
-               var idKey = $scope.companyMarkers.length ? 0 : $scope.companyMarkers.length;
-
-                $scope.companyMarkers.push({
-                    id: idKey,
-                    coords: {
-                    latitude: $scope.marker.markerLat,
-                    longitude: $scope.marker.markerLng
-                  }
-                });
-
-
-
-                console.log('Maker add: ' + $scope.companyMarkers);
-                $scope.markerLat ="";
-                $scope.markerLng ="";
-            };
-
-
-
-            $scope.marker = {
-              id:0,
-              coords: {
-                latitude: 32.7833,
-                longitude: -79.9333
+            var geocoder;
+            var map;
+            $scope.initialize = function () {
+              geocoder = new google.maps.Geocoder();
+              var latlng = new google.maps.LatLng(32.7833, -79.9333);
+              var mapOptions = {
+                zoom: 11,
+                center: latlng
               }
-              }
+              map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+            }
+
+            console.log($scope.geocoder);
+            $scope.codeAddress = function () {
+              var address = $scope.location.address;
+              geocoder.geocode( { '$scope.location.address': address}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                  map.setCenter(results[0].geometry.location);
+                  var marker = new google.maps.Marker({
+                      map: map,
+                      position: results[0].geometry.location
+                  });
+                } else {
+                  alert("Geocode was not successful for the following reason: " + status);
+                }
+              });
+            }
+
 
         }]);
 })();
